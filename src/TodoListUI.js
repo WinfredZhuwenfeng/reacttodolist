6c56b141b8cc7ad1fce8269a61b5f6f17bfbc4b2
WinfredZhuwenfeng <1419657141@qq.com>
2019-05-16 14:54:03 +0800

    '把UI组件改写为无状态组件'

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
-  render() {
-    return(
-      <div>
+
+const TodoListUI = (props) => {
+  return (
+    <div>
         <Input 
-          onChange={this.props.handleChange}
+          onChange={props.handleChange}
           placeholder="Basic usage" 
-          value={this.props.inputValue} 
+          value={props.inputValue} 
           style={{width: '300px'}} />
-        <Button type="primary" onClick={this.props.handleClick}>Primary</Button>
+        <Button type="primary" onClick={props.handleClick}>Primary</Button>
         <List          
           style={{width: '300px'}}
           size="small"
           bordered
-          dataSource={this.props.list}
-          renderItem={(item, index) => <List.Item onClick={(index)=>{this.props.handleDelete(index)}}>{item}</List.Item>}
+          dataSource={props.list}
+          renderItem={(item, index) => <List.Item onClick={(index)=>{props.handleDelete(index)}}>{item}</List.Item>}
         />
       </div>
-    )
-  }
+  )
 }
 
 export default TodoListUI;
